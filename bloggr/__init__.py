66d87056082987ecf984fb345edfed9e0d55084a
Paul Wilson <pfrwilson@gmail.com>
2022-01-10 21:56:33 -0500

    app factory

---
+++ bloggr/__init__.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from .middleware import middleware
+from .routers import auth, blog
+from .db import init_db
+import os
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_DATABASE_URL = "sqlite:///./sql_app.db" 
+
+def get_app(config: dict | None = None):
+    
+    app = FastAPI(middleware = middleware)
+    
+    url = DEFAULT_DATABASE_URL
+    if config: 
+        url = config.get('database_url', DEFAULT_DATABASE_URL)
+    app.state.database_url = url
+    init_db(app)
+    
+    app.mount('/static', StaticFiles(directory=os.path.join(APP_DIR, 'static')), name='static')
+
+    app.include_router(auth.router)
+
+    return app
+
+
+app = get_app()
+
