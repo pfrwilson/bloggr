d90746954796fbb85cfd4383ad481a51abf9e874
Paul Wilson <pfrwilson@gmail.com>
2022-01-11 19:23:47 -0500

    minor change

--- bloggr/middleware.py
+++ bloggr/middleware.py
@@ -3,11 +3,12 @@ from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 
+
 class FlashMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
+    async def dispatch(self, request: Request, call_next):
         request.state.flash = request.session.pop('flash_cache', [])
         response = await call_next(request)
-        return response
+        return response       
 
 def get_flashed_messages(request: Request):
     messages = request.state.flash
@@ -20,10 +21,11 @@ def flash(request: Request, message: str):
     messages.append(message)
     request.session['flash_cache'] = messages
 
-
-
 middleware = [
     Middleware(SessionMiddleware, secret_key = 'example'),
-    Middleware(FlashMiddleware)
+    Middleware(FlashMiddleware),
 ]
+
+
+    
 
